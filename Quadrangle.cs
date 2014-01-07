d4f42bb889aad04517fcb1b9c7bcae557de88d8e
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:38:30 -0600

    initial add

---
+++ Quadrangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomProject
+{
+    class Quadrangle
+    {
+        public Quadrangle()
+        {
+            dLength = 1.0;
+            dWidth = 1.0;
+        }
+
+        public Quadrangle(Double initLength, Double initWidth)
+        {
+            dLength = initLength;
+            dWidth = initWidth;
+        }
+
+        public Double ShowLength()
+        {
+            return dLength;
+        }
+
+        public Double ShowWidth()
+        {
+            return dWidth;
+        }
+
+        public Double ShowArea()
+        {
+            return dWidth * dLength;
+        }
+
+        public Double ShowPerimeter()
+        {
+            return (2 * dLength) + (2 * dWidth);
+        }
+
+        protected Double dLength;
+        protected Double dWidth;
+    }
+}
---
+++ Quadrangle3D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RoomProject
+{
+    class Quadrangle3D : Quadrangle
+    {
+        public Quadrangle3D()
+        {
+            dHeight = 1.0;
+        }
+
+        public Quadrangle3D(Double initHeight)
+        {
+            dHeight = initHeight;
+        }
+
+        public Double ShowBaseArea()
+        {
+            return ShowArea();
+        }
+
+        public Double ShowSurfaceArea()
+        {
+            return (2 * (dLength * dWidth)) + (((2 * dWidth) + (2 * dLength)) * dHeight);
+        }
+
+        public Double ShowVolume()
+        {
+            return dHeight * dLength * dWidth;
+        }
+
+        protected Double dHeight;
+    }
+}
